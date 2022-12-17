1259143a0b651bdd5893783e7b901582a9067d78
Alireza Maleki <mt@gmail.com>
2022-12-17 15:02:47 +0330

    adding loading

---
+++ components/Loading.js
@@ -0,0 +1,9 @@
+export const Loading = () => {
+    return (
+        <div className="spinner" >            
+                <div className='spinnerLoading font-[Vibur] text-[2rem] text-[#4d4d4d]'>
+                    LOADING...
+                </div>
+    </div>
+    )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,27 @@
 import '../styles/globals.css';
 import "animate.css/animate.min.css";
 import ThemeContextProvider from '../context/ThemeContext';
+import { useState , useEffect } from 'react'; 
+import 'react-loading-skeleton/dist/skeleton.css'
+import { Loading } from '../components/Loading.js'
+
+
 
 function MyApp({ Component, pageProps }) {
-  return (
+
+   const [ loading, setLoading ] = useState(true);
+
+   useEffect(() => {
+      setLoading(false);
+    }, []);
+
+   return (
          <>
+           { !loading ? (
          <ThemeContextProvider>
-        <Component {...pageProps} />
+            <Component {...pageProps} />  
          </ThemeContextProvider>
+           ) : (<Loading />)}
          </>
       ) 
 }
